4fc8dd42423b19a559a6428fde7a714932e4a06d
derwentx <derwentx@gmail.com>
2020-06-22 17:28:40 +1000

    cube use vertices

--- src/main/java/heronarts/lx/app/pattern/Panel3DRotatingCube.java
+++ src/main/java/heronarts/lx/app/pattern/Panel3DRotatingCube.java
@@ -49,7 +49,6 @@ public class Panel3DRotatingCube extends Panel3DGraphicsPattern {
     pg.pushMatrix();
     pg.background(LXColor.BLACK);
     pg.stroke(LXColor.WHITE);
-    pg.strokeWeight(0.4f);
     pg.translate(3*model.width/4 - 1f, 2*model.height/3 + 1,0);
     pg.applyMatrix(new PMatrix3D(
       1.f, -0.5f, 0.f, 0.f,
@@ -57,19 +56,16 @@ public class Panel3DRotatingCube extends Panel3DGraphicsPattern {
       0.f, 0.f, 1.f, 0.f,
       0.f, 0.f, 0.f, 1.f
     ));
-    // pg.translate(0, 0, -model.width);
     pg.rotateY((float) totalMs / 1000);
     pg.rotateZ((float) totalMs / 10000);
-    // pg.line(0.f, 0.f, 0.f, model.width, model.height, 0);
-    // for(int i=0; i<edges.size(); i++) {
-    //   PVector from = vertices.get(edges.get(i)[0]);
-    //   PVector to = vertices.get(edges.get(i)[1]);
-    //   pg.line(from.x, from.y, from.z, to.x, to.y, to.z);
-    // }
     pg.noFill();
-    // pg.line(-lineLength, 0, 0, lineLength, 0, 0);
-    // pg.line(0, -lineLength, 0, 0, lineLength, 0);
-    pg.box(8);
+    pg.scale(8);
+    pg.strokeWeight(0.08f);
+    for(int i=0; i<edges.size(); i++) {
+      PVector from = vertices.get(edges.get(i)[0]);
+      PVector to = vertices.get(edges.get(i)[1]);
+      pg.line(from.x, from.y, from.z, to.x, to.y, to.z);
+    }
     pg.popMatrix();
   }
 }
