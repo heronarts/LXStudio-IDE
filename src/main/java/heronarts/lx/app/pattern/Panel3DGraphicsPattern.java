9fda9ad289b9bf01a4ac3cbf4803b74c235fdfe7
derwentx <derwentx@gmail.com>
2020-06-27 13:47:33 +1000

    use mutexes and loopTasks to schedule render

--- src/main/java/heronarts/lx/app/pattern/Panel3DGraphicsPattern.java
+++ src/main/java/heronarts/lx/app/pattern/Panel3DGraphicsPattern.java
@@ -3,6 +3,7 @@ package heronarts.lx.app.pattern;
 import flavius.ledportal.LPPanelModel;
 import heronarts.lx.app.LXStudioApp;
 import heronarts.lx.LX;
+import heronarts.lx.LXLoopTask;
 import heronarts.lx.model.GridModel.Point;
 import heronarts.lx.pattern.LXModelPattern;
 import heronarts.p3lx.P3LX;
@@ -23,79 +24,75 @@ import processing.core.PImage;
  */
 public class Panel3DGraphicsPattern extends LXModelPattern<LPPanelModel> {
   PImage frame;
+  boolean frameReady = false;
   PFont font;
   double totalMs;
-  UIPanel3DGraphicsRenderer renderer;
   PApplet applet;
+  PGraphics pg;
+  LXLoopTask renderTask;
 
   private static final Logger logger = Logger
     .getLogger(Panel3DGraphicsPattern.class.getName());
 
-  class UIPanel3DGraphicsRenderer extends UIObject {
-    public PGraphics pg;
-
-    @Override
-    public float getWidth() {
-      return -1;
-    }
-
-    @Override
-    public float getHeight() {
-      return -1;
-    }
-
-    public UIPanel3DGraphicsRenderer(Panel3DGraphicsPattern pattern) {
-      P3LX lx = (P3LX)pattern.lx;
-      this.pg = lx.ui.applet.createGraphics(
-        pattern.model.width + 1, pattern.model.height + 1, PGraphics.P3D);
-      lx.ui.addLoopTask(new UITimerTask(60, UITimerTask.Mode.FPS) {
-        @Override
-        protected void run() {
-          pattern.beforeDraw(pg);
-          pg.beginDraw();
-          pattern.onDraw(pg);
-          pg.endDraw();
-          pattern.afterDraw(pg);
-        }
-      });
-      logger.info(String.format("pg: %s", this.pg.toString()));
-    }
-  }
-
-  public void beforeDraw(PGraphics pg) {
-    pg.pushStyle();
-  }
+  public void beforeDraw(PGraphics pg) {}
 
   public void onDraw(PGraphics pg) {}
 
-  public void afterDraw(PGraphics pg) {
-    pg.popStyle();
-    this.frame.set(0, 0, pg.get());
-  }
+  public void afterDraw(PGraphics pg) {}
 
   public Panel3DGraphicsPattern(LX lx) {
     super(lx);
-    this.applet = LXStudioApp.instance;
-    this.renderer = new UIPanel3DGraphicsRenderer(this);
-    this.frame = new PImage(model.width + 1, model.height + 1);
+    applet = LXStudioApp.instance;
+    pg = applet.createGraphics(
+        model.width + 1, model.height + 1, PGraphics.P3D);
+    frame = new PImage(pg.width, pg.height);
     String fontPrefix = "Content/fonts/";
     try {
       fontPrefix = lx.getMediaFolder(LX.Media.CONTENT).getCanonicalPath() + "/fonts/";
     } catch (IOException e) {
       logger.severe(String.format("could not get fontPrefix: %s", e.toString()));
     }
-    this.font = this.applet.createFont(fontPrefix + "uni0553-webfont.ttf", 8);
+    font = applet.createFont(fontPrefix + "uni0553-webfont.ttf", 8);
+
+    renderTask = new LXLoopTask() {
+      @Override
+      public void loop(double deltaMs) {
+        synchronized(Panel3DGraphicsPattern.class) {
+          beforeDraw(pg);
+          pg.beginDraw();
+          onDraw(pg);
+          pg.endDraw();
+          afterDraw(pg);
+          frame.set(0, 0, pg.get());
+          frameReady = true;
+        }
+      }
+    };
+
+    synchronized(Panel3DGraphicsPattern.class) {
+      ((P3LX)lx).ui.addLoopTask(renderTask);
+    }
+  }
+
+  @Override
+  public void dispose() {
+    synchronized(Panel3DGraphicsPattern.class) {
+      ((P3LX)this.lx).ui.removeLoopTask(this.renderTask);
+    }
+    super.dispose();
   }
 
   @Override
   public void run(double deltaMs) {
     totalMs += deltaMs;
-    if(frame == null) {
-      logger.info("null frame");
-      return;
-    }
-    for(Point point : model.points) {
-      setColor(point.index, frame.get((model.width - point.xi), (model.height - point.yi)));
+    synchronized(Panel3DGraphicsPattern.class) {
+      if(frameReady == false) {
+        return;
+      }
+      for(Point point : model.points) {
+        setColor(point.index, frame.get((model.width - point.xi), (model.height - point.yi)));
+      }
+      frameReady = false;
     }
   }
 }
