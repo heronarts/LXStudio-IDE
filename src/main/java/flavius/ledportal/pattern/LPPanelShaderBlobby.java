088276d3a3d43c89ab254c67df6b2060a4c9eeed
derwentx <derwentx@gmail.com>
2021-09-27 21:03:48 +0800

    parametrise and add nebula pattern

--- src/main/java/flavius/ledportal/pattern/LPPanelShaderBlobby.java
+++ src/main/java/flavius/ledportal/pattern/LPPanelShaderBlobby.java
@@ -3,11 +3,21 @@ package flavius.ledportal.pattern;
 import heronarts.lx.LX;
 import heronarts.lx.app.LXStudioApp;
 import heronarts.lx.app.media.MediaLibrary;
+import heronarts.lx.parameter.CompoundParameter;
+import heronarts.lx.parameter.LXParameter;
 import processing.core.PConstants;
 import processing.core.PGraphics;
 
 public class LPPanelShaderBlobby extends LPPanelShaderDirect {
     long startTime;
+
+    public final CompoundParameter depth = new CompoundParameter("depth", 1.0,
+        0.0, 2.0).setDescription("Depth of blobbiness")
+            .setPolarity(LXParameter.Polarity.BIPOLAR);
+            
+    public final CompoundParameter rate = new CompoundParameter("rate", 1.0,
+        0.0, 2.0).setDescription("Rate of movement")
+            .setPolarity(LXParameter.Polarity.BIPOLAR);
 
     public LPPanelShaderBlobby(LX lx) {
         super(lx);
@@ -17,17 +27,19 @@ public class LPPanelShaderBlobby extends LPPanelShaderDirect {
         String shaderPath = MediaLibrary.getCanonicalContentPath(lx, "shaders");
         this.shader = LXStudioApp.instance
             .loadShader(shaderPath + "/blobby.glsl");
+
+        addParameter("depth", this.depth);
+        addParameter("rate", this.rate);
     }
 
     @Override
     public void beforeDraw(final PGraphics pg) {
         super.beforeDraw(pg);
         pg.textureMode(PConstants.NORMAL);
-        long time = (System.currentTimeMillis() - startTime);
-        logger.info(String.format("nowMillis, %f", (float) (time / 1000.0)));
-        shader.set("time", (float) (time / 1000.0));
+        shader.set("time",
+            (float) ((System.currentTimeMillis() - startTime) / 1000.0));
         shader.set("resolution", (float) (pg.width), (float) (pg.height));
-        shader.set("depth", (float) (1));
-        shader.set("rate", (float) (1));
+        shader.set("depth", (float) (depth.getValuef()));
+        shader.set("rate", (float) (rate.getValuef()));
     }
 }
---
+++ src/main/java/flavius/ledportal/pattern/LPPanelShaderNebula.java
@@ -0,0 +1,41 @@
+package flavius.ledportal.pattern;
+
+import heronarts.lx.LX;
+import heronarts.lx.app.LXStudioApp;
+import heronarts.lx.app.media.MediaLibrary;
+import heronarts.lx.parameter.LXParameter;
+import heronarts.lx.parameter.CompoundParameter;
+import processing.core.PConstants;
+import processing.core.PGraphics;
+
+public class LPPanelShaderNebula extends LPPanelShaderDirect {
+    long startTime;
+            
+    public final CompoundParameter starspeed = new CompoundParameter("starspeed", 50.0,
+        0.0, 100.0).setDescription("Speed of stars")
+            .setPolarity(LXParameter.Polarity.BIPOLAR);
+
+    public LPPanelShaderNebula(LX lx) {
+        super(lx);
+
+        startTime = System.currentTimeMillis();
+
+        String shaderPath = MediaLibrary.getCanonicalContentPath(lx, "shaders");
+        this.shader = LXStudioApp.instance
+            .loadShader(shaderPath + "/nebula.glsl");
+
+        addParameter("starspeed", this.starspeed);
+    }
+
+    @Override
+    public void beforeDraw(final PGraphics pg) {
+        super.beforeDraw(pg);
+        pg.textureMode(PConstants.NORMAL);
+        shader.set("time", (float) ((System.currentTimeMillis() - startTime) / 1000.0));
+        shader.set("resolution", (float) (pg.width), (float) (pg.height));
+        shader.set("starspeed", (float) (50));
+
+
+        shader.set("starspeed", (float) (starspeed.getValuef()));
+    }
+}
