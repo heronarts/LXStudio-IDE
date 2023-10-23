3c0a4fc1c5d92296dc074122cf7190948a013b9f
Scott <schott12521@gmail.com>
2023-10-23 00:34:47 -0700

    Fixing LXStudio-IDE for macOS Sonoma

--- src/main/java/heronarts/lx/app/LXStudioApp.java
+++ src/main/java/heronarts/lx/app/LXStudioApp.java
@@ -163,6 +163,12 @@ public class LXStudioApp extends PApplet implements LXPlugin {
       System.getProperty("os.version") + " " +
       System.getProperty("os.arch")
     );
+
+
+    // NOTE(mcslee): Hack for macOS Sonoma!!
+    // Hopefully to be removed in a future version
+    com.jogamp.opengl.GLProfile.initSingleton();
+
     boolean headless = false;
     File projectFile = null;
     for (int i = 0; i < args.length; ++i) {
